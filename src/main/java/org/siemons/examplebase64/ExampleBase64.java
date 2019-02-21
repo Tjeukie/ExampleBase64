384ec8ae6b929323a38b9ba0428bc933d83a0773
Martjo Siemons <m.siemons@advanced.nl>
2019-02-21 18:22:01 +0100

    My second initial add

--- src/main/java/org/siemons/examplebase64/ExampleBase64.java
+++ src/main/java/org/siemons/examplebase64/ExampleBase64.java
@@ -5,7 +5,7 @@ import java.util.Base64;
 public class ExampleBase64 {
     
     public static void main(String[] args) {
-        String code = "Hi die Tjeu";
+        String code = "Hi Tjeu";
         System.out.println("Before encoding: " + code);
         
         byte[] encodedBytes = Base64.getEncoder().encode(code.getBytes());

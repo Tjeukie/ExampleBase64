dec1b4f187e103010992c9bfdb1e94a24a9f865d
Martjo Siemons <m.siemons@advanced.nl>
2019-02-21 17:28:32 +0100

    My second initial add

---
+++ src/main/java/org/siemons/examplebase64/ExampleBase64.java
@@ -0,0 +1,20 @@
+package org.siemons.examplebase64;
+
+import java.util.Base64;
+
+public class ExampleBase64 {
+    
+    public static void main(String[] args) {
+        String code = "Hi Tjeu";
+        System.out.println("Before encoding: " + code);
+        
+        byte[] encodedBytes = Base64.getEncoder().encode(code.getBytes());
+        code = new String(encodedBytes);
+        System.out.println("After encoding and before decoding: " + code);
+
+        code = new String(Base64.getDecoder().decode(code));
+        System.out.println("After decoding: " + code);
+
+    }
+
+}
